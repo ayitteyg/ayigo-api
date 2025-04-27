c010e5ccb9e4a529253f5711d48c9c2780b4bd9d
ayitteyg <ayitteyg@yahoo.com>
2025-04-27 13:44:57 +0000

    chore: modifies debugging database

--- project/settings.py
+++ project/settings.py
@@ -177,6 +177,9 @@ DATABASES = {
     )
 }
 
+#debugging database ..
+print("Database Settings:", DATABASES['default'])
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
